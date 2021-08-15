88438316cfaebd4ad9e800b94c99faff40ffd9c0
builder <kessirin296@gmail.com>
2021-08-15 13:15:36 +0700

    Add add cdd to informatiom bar

--- src/components/common/InfomationBar.tsx
+++ src/components/common/InfomationBar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const InfomationBar = () => {
 
-    return <div>
+    return <div className="bg-gradient-to-r from-green-400 to-blue-500">
         Hallo! Ich bin Builder!
     </div>
 }
