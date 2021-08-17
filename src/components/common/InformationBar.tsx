d347ab06c279eaba51b738d704dae374f1f15e18
builder <kessirin296@gmail.com>
2021-08-17 23:24:58 +0700

    Update header style

--- src/components/common/InformationBar.tsx
+++ src/components/common/InformationBar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const InformationBar = () => {
 
-    return <div className="bg-gradient-to-r from-green-400 to-blue-500">
+    return <div className="bg-gradient-to-r from-green-400 to-blue-500 py-1 text-white">
         Hallo! Ich bin Builder!
     </div>
 }
--- src/components/menu-bar/MenuBar.tsx
+++ src/components/menu-bar/MenuBar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const MenuBar = () => {
 
-    return <div>
+    return <div className="py-2 h-10">
         Menu bar & Always hold on top page
     </div>
 }
