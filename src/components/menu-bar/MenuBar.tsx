eabf892a2a08913060254c70af040361eafb8d7b
builder <kessirin296@gmail.com>
2021-08-19 22:40:43 +0700

    Add sticky for menu bar

--- src/components/menu-bar/MenuBar.tsx
+++ src/components/menu-bar/MenuBar.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const MenuBar = () => {
 
-    return <div className="py-2 h-10">
-        Menu bar & Always hold on top page
+    return <div className="py-3 h-12 sticky top-0 max-w-8xl mx-auto bg-white flex-none flex">
+        <p>Menu bar & Always hold on top page</p>
     </div>
 }
 
